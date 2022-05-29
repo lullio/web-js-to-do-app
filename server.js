2b08faa827333502a6cda8569b046cde5154a288
Felipe IntelSantos <felipelullio@gmail.com>
2022-05-29 18:17:44 -0300

    express e mongodb config do servidor e acesso bd
    conexão com banco de dados e configuração do express

---
+++ server.js
@@ -0,0 +1,19 @@
+let express = require("express");
+let {MongoClient} = require('mongodb');
+
+
+let myApp = express();
+
+let bd;
+
+async function go(){
+  let client = new MongoClient('mongodb+srv://admin:admin@cluster0.6arhq.mongodb.net/?retryWrites=true&w=majority');
+  await client.connect();
+  bd = client.db('Lista-Tarefas-Felipe');
+    
+  myApp.listen(3000, "0.0.0.0", (e) => {
+    console.log("listening on port 3000: ");
+    console.log(e.log);  
+  });
+}
+
